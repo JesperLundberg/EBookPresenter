58eb4c31962c113146e43711db1d359223ab6bc8
Jesper Lundberg <lundberg.jesper@gmail.com>
2021-07-10 22:39:06 +0200

    Finish buildDocker-script

--- src/EBookPresenter/docker/buildDocker.py
+++ src/EBookPresenter/docker/buildDocker.py
@@ -12,15 +12,29 @@ def folder_exists(path):
     else:
         return False
 
+def clean_build_folder(path):
+	command = "rm -rf " + path
+	os.system(command)
+
 def copy_source_code():
     print("Copying source code")
-    command = ['rsync', '-av', '../','./build']
+    command = ['rsync', '-av', '../','./build', 
+    '-exclude bin', '-exclude obj']
     result = subprocess.run(command, stdout=subprocess.PIPE)
     return result.stdout
+
+def build_docker():
+    print("Building docker file")
+    command = "docker build -t ebookpresenter:0.0.1-rc1 build/"
+    os.system(command)
 
 # Actual script
 
 if not folder_exists("build/"):
     create_folder("build/")
+else:
+	clean_build_folder("build/")
+	create_folder("build/")
 
 copy_source_code()
+build_docker()
