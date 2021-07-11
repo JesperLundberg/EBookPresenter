c4839e02969375dac524ccb6ee73cf5b50400e01
Jesper Lundberg <lundberg.jesper@gmail.com>
2021-07-12 00:20:35 +0200

    Add cleanDocker with removal of container implemented

--- src/EBookPresenter/docker/cleanDocker.py
+++ src/EBookPresenter/docker/cleanDocker.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 
+import subprocess
+from dockerUtilities import get_value
+
+def does_container_exist(container_name):
+    command = ['docker', 'ps', \
+    '--filter "name=' + container_name +'"', '-q']
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+
+    if not result:
+        return True
+    else:
+        return False
+
+print(does_container_exist(get_value("name")))
