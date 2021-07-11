35b8be3dc6cd460a4da58cc04b65bc4e9f886e27
Jesper Lundberg <lundberg.jesper@gmail.com>
2021-07-12 01:40:42 +0200

    Docker commands now work

--- src/EBookPresenter/docker/cleanDocker.py
+++ src/EBookPresenter/docker/cleanDocker.py
@@ -4,13 +4,22 @@ import subprocess
 from dockerUtilities import get_value
 
 def does_container_exist(container_name):
-    command = ['docker', 'ps', \
-    '--filter "name=' + container_name +'"', '-q']
-    result = subprocess.run(command, stdout=subprocess.PIPE)
+	command = ['docker', 'ps', '--filter name=' + container_name, '-q']
+	result = subprocess.run(command, stdout=subprocess.PIPE)
+	
+	if not result.stdout:
+		return False
+	else:
+		return True
 
-    if not result:
-        return True
-    else:
-        return False
+def does_image_exist(image_name):
+	command = ['docker', 'images', image_name, '-q']
+	result = subprocess.run(command, stdout=subprocess.PIPE)
+
+	if not result.stdout:
+		return False
+	else:
+		return True
 
 print(does_container_exist(get_value("name")))
+print(does_image_exist(get_value("name") + ":" + get_value("release")))
