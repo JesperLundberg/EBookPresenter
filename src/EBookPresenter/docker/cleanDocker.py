e053d78074c69e66855127e0b5cf75da012aef80
Jesper Lundberg <lundberg.jesper@gmail.com>
2021-07-13 14:47:11 +0200

    Add removal of container method

--- src/EBookPresenter/docker/cleanDocker.py
+++ src/EBookPresenter/docker/cleanDocker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import subprocess
+import os
 from dockerUtilities import get_value
 
 def does_container_exist(container_name):
@@ -21,5 +22,17 @@ def does_image_exist(image_name):
 	else:
 		return True
 
+def remove_container(container_name):
+	command = "docker stop " + container_name
+	result = os.system(command)
+
+	if result == 0:
+		command = "docker rm -f " + container_name
+		result = os.system(command)
+
+	return result
+
 print(does_container_exist(get_value("name")))
 print(does_image_exist(get_value("name") + ":" + get_value("release")))
+
+#print(remove_container(get_value("name")))
