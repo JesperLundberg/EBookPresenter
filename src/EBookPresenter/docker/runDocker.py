7c7a04fce25a10292fe485b90c2aa25e75a6c2a3
Jesper Lundberg <lundberg.jesper@gmail.com>
2021-07-14 13:55:01 +0200

    runDocker now starts docker correctly

--- src/EBookPresenter/docker/runDocker.py
+++ src/EBookPresenter/docker/runDocker.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 
+import os
+from dockerUtilities import get_value
+from dockerUtilities import container_exist
+from dockerUtilities import remove_container
+
+def run_docker(container_name, release, path_to_bind):
+    command = "docker run -d -p 5000:80 --mount type=bind,source=" +\
+    path_to_bind +\
+    ",target=/books,readonly --name " + container_name + " " +\
+    container_name + ":" + release
+    result = os.system(command)
+
+    return result
+
+if(container_exist(get_value("name"))):
+    remove_container(get_value("name"))
+
+run_docker(get_value("name"), get_value("release"), "/mnt/c/temp/")
