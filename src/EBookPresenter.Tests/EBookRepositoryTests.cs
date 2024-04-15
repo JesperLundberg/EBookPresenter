2400c224b8b2b43922772082d240c71b73b59903
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-04-15 10:18:16 +0200

    change to dotnet version 8 and clean up

--- src/EBookPresenter.Tests/EBookRepositoryTests.cs
+++ src/EBookPresenter.Tests/EBookRepositoryTests.cs
@@ -1,12 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
-
 using EBookPresenter.Models;
 using EBookPresenter.Repositories;
 using EBookPresenter.Tests.Comparers;
 using EBookPresenter.Tests.Fakes;
 using EBookPresenter.Tests.TestHelpers;
-
 using NUnit.Framework;
 
 namespace EBookPresenter.Tests;
@@ -14,73 +12,47 @@ namespace EBookPresenter.Tests;
 public class EBookRepositoryTests
 {
     [SetUp]
-    public void Setup()
-    {
-    }
+    public void Setup() { }
 
     [Test]
     public void EBookRepository_GetAllEBooksRecursive_Returns_All_Ebooks_In_The_Given_Path()
     {
-        var ebookRepository = new EBookRepository(new FileSystemFake(),
-            new FileInfoFactoryFake());
+        var ebookRepository = new EBookRepository(new FileSystemFake(), new FileInfoFactoryFake());
 
         var result = ebookRepository.GetEbooksRecursive("folderDoesntMatter/");
 
-        Assert.AreEqual(8,
-            result.Count());
+        Assert.AreEqual(8, result.Count());
     }
 
     [Test]
     public void EBookRepository_OrderBooks_Alphabetic_Returns_Books_Ordered_By_Alphabetic_Order_Ascending()
     {
-        var ebookRepository = new EBookRepository(new FileSystemFake(),
-            new FileInfoFactoryFake());
-
-        var unorderedBookList = CreateBookObjects.GetUnorderedEBookList();
+        var ebookRepository = new EBookRepository(new FileSystemFake(), new FileInfoFactoryFake());
 
         var expected = new List<EBook>
         {
-            new ()
-            {
-                Title = "Title1"
-            },
-            new ()
-            {
-                Title = "Title2"
-            },
-            new ()
-            {
-                Title = "Title3"
-            },
-            new ()
-            {
-                Title = "Title4"
-            }
+            new() { Title = "Title1" },
+            new() { Title = "Title2" },
+            new() { Title = "Title3" },
+            new() { Title = "Title4" }
         };
 
-        var result = ebookRepository.OrderBooks(unorderedBookList,
-            "alphabetic");
+        var unorderedBookList = CreateBookObjects.GetUnorderedEBookList();
+        var result = ebookRepository.OrderBooks(unorderedBookList, "alphabetic");
 
-        CollectionAssert.AreEqual(expected,
-            result,
-            new TitleComparer());
+        CollectionAssert.AreEqual(expected, result, new TitleComparer());
     }
 
     [Test]
     public void EBookRepository_OrderBooks_Created_Returns_Books_Ordered_By_Creation_Date_Ascending()
     {
-        var ebookRepository = new EBookRepository(new FileSystemFake(),
-            new FileInfoFactoryFake());
+        var ebookRepository = new EBookRepository(new FileSystemFake(), new FileInfoFactoryFake());
 
         var unorderedBookList = CreateBookObjects.GetUnorderedEBookList().ToList();
-
         var expected = unorderedBookList.OrderByDescending(x => x.CreatedDate);
 
-        var result = ebookRepository.OrderBooks(unorderedBookList,
-            "creation");
+        var result = ebookRepository.OrderBooks(unorderedBookList, "creation");
 
-        CollectionAssert.AreEqual(expected,
-            result,
-            new CreationDateComparer());
+        CollectionAssert.AreEqual(expected, result, new CreationDateComparer());
     }
 }
